95a00c62ea57c006e41b9a69cad353b37110a11d
Diego Rodriguez <diego25598@gmail.com>
2023-11-10 11:55:39 +0100

    Add MatRadioModule in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCardModule, MatIconModule, MatInputModule, MatTableModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatCheckboxModule, MatIconModule, MatInputModule, MatRadioModule, MatTableModule, MatToolbarModule } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
 import { ContactHomeComponent } from './contact-home/contact-home.component';
 import { ContactDetailComponent } from './contact-detail/contact-detail.component';
@@ -11,17 +10,27 @@ import { ProductHomeComponent } from './product-home/product-home.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ContactNewComponent } from './contact-new/contact-new.component';
 import { FormsModule } from '@angular/forms';
+import { ProductNewComponent } from './product-new/product-new.component';
+import { MatSelectModule } from '@angular/material/select';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
+
   declarations: [
+
     AppComponent,
     ContactHomeComponent,
     ContactDetailComponent,
     ProductHomeComponent,
     ProductDetailComponent,
     ContactNewComponent,
+    ProductNewComponent,
+
   ],
+
   imports: [
+
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -32,9 +41,15 @@ import { FormsModule } from '@angular/forms';
     MatCardModule,
     MatIconModule,
     FormsModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule
+
   ],
+
   providers: [],
   bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }

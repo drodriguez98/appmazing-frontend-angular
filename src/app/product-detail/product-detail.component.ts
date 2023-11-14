cf263f7a0f914037b8a1c4bdff86af5762839321
Diego Rodriguez <diego25598@gmail.com>
2023-11-14 20:35:38 +0100

    Add methods to edit product and close section

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../products.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
 
@@ -14,12 +14,12 @@ export class ProductDetailComponent implements OnInit {
 
   product: any;
 
-  constructor(private productsService: ProductsService, private route: ActivatedRoute) {}
+  constructor(private productsService: ProductsService, private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() { this.productsService.getProduct(this.route.snapshot.params['id']).subscribe(data => { this.product = data }) }
 
-    this.productsService.getProduct(this.route.snapshot.params['id']).subscribe(data => { this.product = data })
+  editProduct() { this.router.navigate(['/product/edit', this.route.snapshot.params['id']]) }
 
-  }
+  closeProduct() { this.router.navigate(['/products']); }
 
 }

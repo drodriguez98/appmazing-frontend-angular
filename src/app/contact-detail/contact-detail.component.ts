3e4ac93a2b3d58f588aaae9cafa937d2515af533
Diego Rodriguez <diego25598@gmail.com>
2023-11-14 17:11:39 +0100

    Add edit and close contact details methods

--- src/app/contact-detail/contact-detail.component.ts
+++ src/app/contact-detail/contact-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactsService } from '../contacts.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
 
@@ -13,12 +13,12 @@ export class ContactDetailComponent implements OnInit {
 
   contact: any;
 
-  constructor(private contactsService: ContactsService, private route: ActivatedRoute) {}
+  constructor(private contactsService: ContactsService, private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit() {
-    
-    this.contactsService.getContact(this.route.snapshot.params['id']).subscribe(data => { this.contact = data })
+  ngOnInit() { this.contactsService.getContact(this.route.snapshot.params['id']).subscribe(data => { this.contact = data }) }
 
-  }
+  editContact() { this.router.navigate(['/contact/edit', this.route.snapshot.params['id']]) }
+
+  closeContact() { this.router.navigate(['/contacts']); }
 
 }

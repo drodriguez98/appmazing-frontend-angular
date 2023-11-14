dd9f6585148c3f4d547c5d90448e29db95d8fa18
Diego Rodriguez <diego25598@gmail.com>
2023-11-14 17:05:45 +0100

    Add path route to contactEditComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,18 @@ import { ProductHomeComponent } from './product-home/product-home.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ContactNewComponent } from './contact-new/contact-new.component';
 import { ProductNewComponent } from './product-new/product-new.component';
+import { ContactEditComponent } from './contact-edit/contact-edit.component';
 
 const routes: Routes = [
 
   { path: 'contacts', component: ContactHomeComponent },
   { path: 'contact/new', component: ContactNewComponent },
   { path: 'contact/:id', component: ContactDetailComponent },
+  { path: 'contact/edit/:id', component: ContactEditComponent },
   { path: 'products', component: ProductHomeComponent },
   { path: 'product/new', component: ProductNewComponent },
-  { path: 'product/:id', component: ProductDetailComponent }
-
+  { path: 'product/:id', component: ProductDetailComponent },
+  
 ];
 
 @NgModule({

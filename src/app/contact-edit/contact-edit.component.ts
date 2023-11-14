0b4c8f6c5bffe911e6548d04582b5e084c5190e5
Diego Rodriguez <diego25598@gmail.com>
2023-11-14 17:10:15 +0100

    Add contact edit component

---
+++ src/app/contact-edit/contact-edit.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ContactsService } from '../contacts.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-contact-edit',
+  templateUrl: './contact-edit.component.html',
+  styleUrls: ['./contact-edit.component.css']
+})
+export class ContactEditComponent implements OnInit {
+
+  contact: any;
+
+  constructor(private contactService: ContactsService, private route: ActivatedRoute, private router: Router) {} 
+
+  ngOnInit() { this.contactService.getContact(this.route.snapshot.params['id']).subscribe(data => { this.contact = data; })}
+
+  editContact(): void {
+
+    this.contactService.editContact(this.contact);
+    this.navigateToDetail();
+
+  }
+
+  cancelEdit() { this.navigateToDetail(); }
+
+  navigateToDetail() { this.router.navigate(['/contact', this.route.snapshot.params['id']])};
+
+}
